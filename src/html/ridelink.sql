b6fc85876ca7d8b8b039e2a674a60d9559c22966
Iyesiam <zawahwi10@gmail.com>
2024-05-02 15:25:36 +0200

    recent database

--- src/html/ridelink.sql
+++ src/html/ridelink.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 29, 2024 at 12:57 PM
+-- Generation Time: May 02, 2024 at 03:21 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -43,11 +43,8 @@ CREATE TABLE `booking_details` (
 --
 
 INSERT INTO `booking_details` (`booking_id`, `driver_id`, `driver_name`, `pickup_location`, `destination_location`, `booking_time`, `status`, `user_id`) VALUES
-(1, 17, 'test3', 'Nyabugogo', 'Kimironko', '2024-04-23 11:08:53', 'pending', 0),
-(4, 17, 'test3', 'Nyabugogo', 'masaka', '2024-04-23 12:11:08', 'pending', 0),
-(5, 17, 'test3', 'kimironko', 'masaka', '2024-04-23 12:17:54', 'pending', 15),
-(6, 17, 'test3', 'kicukiro', 'remera', '2024-04-23 12:25:59', 'pending', 15),
-(7, 17, 'test3', 'kicukiro', 'remera', '2024-04-28 15:17:26', 'pending', 17);
+(1, 17, 'test3', 'Nyabugogo', 'Kimironko', '2024-04-23 11:08:53', 'accepted', 0),
+(9, 17, 'test3', 'this ', 'that ', '2024-05-01 14:40:19', 'accepted', 15);
 
 -- --------------------------------------------------------
 
@@ -61,21 +58,19 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` varchar(50) NOT NULL,
-  `registration_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `latitude` decimal(10,8) DEFAULT 0.00000000,
-  `longitude` decimal(11,8) DEFAULT 0.00000000
+  `registration_date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `name`, `email`, `password`, `role`, `registration_date`, `latitude`, `longitude`) VALUES
-(15, 'test', 'test@gmail.com', '$2y$10$/R.gjQ5HsFUAIeM0FM3Z7Ohx.mGM0sglpN50NmICk7v2nvlvd3kX6', 'passenger', '2024-04-16 13:46:39', 0.00000000, 0.00000000),
-(16, 'User2', 'Test2@gmail.com', '$2y$10$03k44jv49JtTeLzDfXAEm.Zea99nAbWHYfAkMA8VkVkfObCJCbukS', 'temporary_driver', '2024-04-16 14:11:30', 0.00000000, 0.00000000),
-(17, 'test3', 'test3@gmail.com', '$2y$10$wPLc5PKDzB0L3...nItfXuqqA4in.kM4NRaYMitLlySip2b5xMCYK', 'private_driver', '2024-04-16 18:18:26', 0.00000000, 0.00000000),
-(25, 'mybigbruh', 'mybigbruh@gmail.com', '$2y$10$jxFLxiukJkJmZ2cWcrm6B.za9aBI3UHw13CoTSPx1GEC.eSP7SY7G', 'temporary_driver', '2024-04-17 08:29:20', 0.00000000, 0.00000000),
-(26, 'yourbigbruh', 'yourbigbruh@gmail.com', '$2y$10$ZSD.1X4B7qZ31uNudCGJkO.k3muLi7QbpTq0wXMhVUurE/OdyURrW', 'temporary_driver', '2024-04-17 08:32:28', 0.00000000, 0.00000000);
+INSERT INTO `users` (`user_id`, `name`, `email`, `password`, `role`, `registration_date`) VALUES
+(15, 'test', 'test@gmail.com', '$2y$10$/R.gjQ5HsFUAIeM0FM3Z7Ohx.mGM0sglpN50NmICk7v2nvlvd3kX6', 'passenger', '2024-04-16 13:46:39'),
+(16, 'User2', 'Test2@gmail.com', '$2y$10$03k44jv49JtTeLzDfXAEm.Zea99nAbWHYfAkMA8VkVkfObCJCbukS', 'temporary_driver', '2024-04-16 14:11:30'),
+(17, 'test3', 'test3@gmail.com', '$2y$10$wPLc5PKDzB0L3...nItfXuqqA4in.kM4NRaYMitLlySip2b5xMCYK', 'private_driver', '2024-04-16 18:18:26'),
+(25, 'mybigbruh', 'mybigbruh@gmail.com', '$2y$10$jxFLxiukJkJmZ2cWcrm6B.za9aBI3UHw13CoTSPx1GEC.eSP7SY7G', 'temporary_driver', '2024-04-17 08:29:20'),
+(26, 'yourbigbruh', 'yourbigbruh@gmail.com', '$2y$10$ZSD.1X4B7qZ31uNudCGJkO.k3muLi7QbpTq0wXMhVUurE/OdyURrW', 'temporary_driver', '2024-04-17 08:32:28');
 
 -- --------------------------------------------------------
 
@@ -103,20 +98,6 @@ INSERT INTO `user_documents` (`document_id`, `user_id`, `id_document`, `driving_
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_locations`
---
-
-CREATE TABLE `user_locations` (
-  `location_id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `latitude` decimal(10,8) DEFAULT NULL,
-  `longitude` decimal(11,8) DEFAULT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user_sessions`
 --
 
@@ -134,14 +115,8 @@ CREATE TABLE `user_sessions` (
 --
 
 INSERT INTO `user_sessions` (`session_id`, `user_id`, `session_token`, `status`, `start_time`, `end_time`) VALUES
-(29, 15, 'b65a25afba3309166f534d41be08f6456ff10fa88fc21cf07da1ce8f195f9f8b', 'disconnected', '2024-04-25 09:28:18', NULL),
-(30, 17, '9f0f866deeac1397ad9253d2aacbb43d25473361946daa19dadeb0fbb82a0696', 'disconnected', '2024-04-25 09:28:40', NULL),
-(31, 15, '21321e107bc1fb221ca02e1fbb4648184dbff29c7412133ff5263a37b2f8918a', 'disconnected', '2024-04-25 13:14:01', NULL),
-(32, 15, '717948faa16bd2aae546678d84ef37870f46ebb2e16fed6a2738f082da66492a', 'active', '2024-04-28 15:13:01', NULL),
-(33, 15, 'ced85ab41211c31d69d81dd2a7961e53540541f4d2408022c22e8ab02382efc5', 'active', '2024-04-28 15:14:06', NULL),
-(34, 17, '21b3fe6ada38dda7cf2cb31709f0a3df2b227ccf4e9887131c07da70c52e2382', 'disconnected', '2024-04-28 15:16:32', NULL),
-(35, 15, '8336aee1cf26329be996ab1a664465df2bf0b1035cd3319a12e38df1b4e4f393', 'active', '2024-04-28 16:31:18', NULL),
-(36, 17, '222558e33b1fd1e3258f06b40f0ce0f92fc0c91b4ce0ee000368c553a44f964c', 'disconnected', '2024-04-28 16:32:26', NULL);
+(58, 15, '6dc2188d6e90795a767c867a05248e73517b044ee64f1b764980455f42237072', 'active', '2024-05-02 13:14:53', NULL),
+(59, 17, 'd1318eabcbb6362aec75db6a3203147cdbef3bcf703d855139a6e63838d550c7', 'active', '2024-05-02 13:16:04', NULL);
 
 --
 -- Indexes for dumped tables
@@ -168,12 +143,6 @@ ALTER TABLE `user_documents`
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indexes for table `user_locations`
---
-ALTER TABLE `user_locations`
-  ADD PRIMARY KEY (`location_id`);
-
---
 -- Indexes for table `user_sessions`
 --
 ALTER TABLE `user_sessions`
@@ -187,7 +156,7 @@ ALTER TABLE `user_sessions`
 -- AUTO_INCREMENT for table `booking_details`
 --
 ALTER TABLE `booking_details`
-  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -202,16 +171,10 @@ ALTER TABLE `user_documents`
   MODIFY `document_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `user_locations`
---
-ALTER TABLE `user_locations`
-  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `user_sessions`
 --
 ALTER TABLE `user_sessions`
-  MODIFY `session_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `session_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
 -- Constraints for dumped tables
